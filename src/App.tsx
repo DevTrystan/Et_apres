c43d0a3e90d3ab837646e0206845e78f6d1a0d78
Roland <leteneur.roland@hotmail.fr>
2022-07-08 15:47:00 +0200

    init homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Redirect, Switch, BrowserRouter, Route } from 'react-router-dom';
-import { OtherPage, ProfilPage } from './pages';
+import { HomePage, OtherPage, ProfilPage } from './pages';
 
 const Routes: React.FC<unknown> = () => {
 	return (
 		<div className="relative">
 			<Switch>
-				<Redirect exact from="/" to="/profile" />
-				<Redirect exact from="*undefined*" to="/me" />
+				<Redirect exact from="/" to="/home" />
+				<Redirect exact from="*undefined*" to="/home" />
 
+				<Route path="/home" component={HomePage} />
 				<Route path="/me" component={ProfilPage} />
 				<Route path="/other" component={OtherPage} />
 			</Switch>
