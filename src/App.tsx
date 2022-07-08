57e3632ff99515221d94fd44b19c5d2f4b93c8be
Roland <leteneur.roland@hotmail.fr>
2022-07-08 14:27:38 +0200

    testing router :construction:

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import { Redirect, Switch, BrowserRouter, Route } from 'react-router-dom';
-import { ProfilPage } from './pages';
+import { OtherPage, ProfilPage } from './pages';
 
 const Routes: React.FC<unknown> = () => {
-  return (
-    <div className="relative">
-      <Switch>
-        <Redirect exact from="/" to="/profile" />
-        <Redirect exact from="*undefined*" to="/profile" />
+	return (
+		<div className="relative">
+			<Switch>
+				<Redirect exact from="/" to="/profile" />
+				<Redirect exact from="*undefined*" to="/me" />
 
-        <Route path="/profile" component={ProfilPage} />
-      </Switch>
-    </div>
-  );
+				<Route path="/me" component={ProfilPage} />
+				<Route path="/other" component={OtherPage} />
+			</Switch>
+		</div>
+	);
 };
 
 const App: React.FC<unknown> = () => {
-  return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <Routes />
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter basename={import.meta.env.BASE_URL}>
+			<Routes />
+		</BrowserRouter>
+	);
 };
 
 export default App;
